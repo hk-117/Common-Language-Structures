f6fab8dd12f74492c313908ae57cbfd292073a0f
hk_117 <zubayer11704@gmail.com>
2023-03-10 14:13:41 +0600

    PHP Language added

---
+++ PHP/01_output_showing.php
@@ -0,0 +1,6 @@
+<?php
+    $name = 'Zubayer';
+    echo "Hello $name\n";
+    echo 'Hello $name\n'."\n";
+    printf("Hello $name\nWeight:%d\n",10);
+?>
---
+++ PHP/02_variables_types.php
@@ -0,0 +1,7 @@
+<?php
+    $name = 'Smith';
+    $age = 62;
+    $weight = 63.5;
+    $married = true;
+    printf("Name: $name\nAge:$age\nWeight:%.2f\nMarried:$married\n",$weight);
+?>
