5e753e2d33790f41158a80f59d1353caad26f33b
hk_117 <zubayer11704@gmail.com>
2023-03-10 19:41:06 +0600

    PHP comparison operators

---
+++ PHP/06_operators_comparison.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 20;
+    $b = "25";
+    printf("$a == $b : %d\n",$a==$b);
+    printf("$a != $b : %d\n",$a!=$b);
+    printf("$a <> $b : %d\n",$a<>$b);
+    printf("$a >= $b : %d\n",$a>=$b);
+    printf("$a <= $b : %d\n",$a<=$b);
+    printf("25 == $b : %d\n",25==$b);
+    printf("25 === $b : %d\n",25===$b);
+?>
