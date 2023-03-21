766b1aaf049742d3c5846f5d62d98c5f3f31f93e
hk_117 <zubayer11704@gmail.com>
2023-03-21 12:20:15 +0600

    Assignment and Logical Operators

---
+++ PHP/07_operators_assignment.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 20;
+    $b = "25";
+    printf("$a += $b : %d\n",$a+=$b);
+    printf("$a -= $b : %d\n",$a-=$b);
+    printf("$a *= $b : %d\n",$a*=$b);
+    printf("$a /= $b : %d\n",$a/=$b);
+    printf("$a %%= $b : %d\n",$a%=$b);
+?>
---
+++ PHP/08_operators_logical.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 1;
+    $b = 0;
+    printf("$a and $b : %d\n",$a and $b);
+    printf("$a or $b : %d\n",$a or $b);
+    printf("$a xor $b : %d\n",$a xor $b);
+    printf("$a && $b : %d\n",$a && $b);
+    printf("$a || $b : %d\n",$a || $b);
+    printf("$a ^ $b : %d\n",$a ^ $b);
+    printf(" !$a : %d\n",!$a);
+?>
